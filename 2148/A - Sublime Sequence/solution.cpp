43eb9da387e0a3cfb730e12dba1d29310e9e5d94
hazem <142867645+hazemism@users.noreply.github.com>
2025-09-13 17:44:52 +0300

    Add Sublime Sequence [A] from Codeforces

---
+++ 2148/A - Sublime Sequence/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std;#define ll long long  int main(){    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);    int t; cin>>t;    while (t--){        int n,x; cin>>n>>x;        if (x%2==0){cout <<0<<endl;continue;}        cout << n << endl;    }      return 0;}
