5de7eeee5e5b45fe3865d6266444063f2c00a3fd
hazem <142867645+hazemism@users.noreply.github.com>
2026-01-03 01:45:47 +0200

    Time: 36 ms (5.05%) | Memory: 35.2 MB (6.48%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        map <int,int> mp; int x;
+        for (int i=0; i<nums.size();i++){
+            mp[nums[i]]++;
+            if (mp[nums[i]]==(nums.size()/2)){x=nums[i];}
+        }
+        return x;
+    }
+};
