96607e269b7331f51f050cbb61a9c048a1edda13
hazem <142867645+hazemism@users.noreply.github.com>
2026-01-03 01:47:09 +0200

    Time: 39 ms (5.05%) | Memory: 35.3 MB (6.48%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.cpp
@@ -4,7 +4,7 @@ public:
         map <int,int> mp; int x;
         for (int i=0; i<nums.size();i++){
             mp[nums[i]]++;
-            if (mp[nums[i]]==(nums.size()/2)){x=nums[i];}
+            if (mp[nums[i]]==(nums.size()/2)){x=nums[i];break;}
         }
         return x;
     }
